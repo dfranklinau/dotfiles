848fa1798670f6b1c4bc3c3ee4111a46f46fa696
Daniel Franklin <git@danielfranklin.id.au>
2017-06-08 17:40:38 +1000

    Reformat and add a default theme to Emacs
    The default package archive was updated from MELPA stable to MELPA. Vim
    keybindings for Emacs were enabled using `evil-mode` and the colour
    theme Zenburn was added.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,8 @@
-;; Set up the MELPA repository
+;; Set up the MELPA repository.
 ;; http://melpa.org/#/getting-started
 (require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
@@ -11,22 +10,36 @@
 (package-initialize)
 
 
+;; Add Vim keybindings to Emacs.
+(require 'evil)
+(evil-mode 1)
+
+
+;; Set the lisp system and, optionally, some contribs.
+;; https://github.com/slime/slime#quick-setup-instructions
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(setq slime-contribs '(slime-fancy))
+
+
+;; Add the Zenburn theme for Emacs
+;; https://github.com/bbatsov/zenburn-emacs
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (slime))))
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("4528fb576178303ee89888e8126449341d463001cb38abe0015541eb798d8a23" default)))
+ '(package-selected-packages (quote (evil slime))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
-
-
-;; Set the lisp system and, optionally, some contribs
-;; https://github.com/slime/slime#quick-setup-instructions
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
-(setq slime-contribs '(slime-fancy))
+ )
